7d7df79ac1342496070bf4bcaaf139b116d24f71
7Sergey7 <stepanyansergey7@gmail.com>
2022-06-13 23:38:26 +0400

    Initial Commit

---
+++ Lesson 5/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  Lesson 5
+//
+//  Created by Seroj on 04.05.22.
+//
+
+import Foundation
+
+//var a = "s"
+//
+//switch a {
+//case "a":
+//    print("First")
+//case "z":
+//    print("Last")
+//default:
+//    print("Ather character")
+//}
+//
+//let age = 15
+//
+//switch age {
+//case 15, 18:
+//    print(age)
+//default:
+//    print("ok")
+//}
+//
+//let point = (x: 3, y: 6)
+//
+//switch point {
+//case (3, 3):
+//    print("ok")
+//case (_, 6):
+//    print("y = 6")
+//case(4, _):
+//    print("x = 4")
+//default:
+//    print("")
+//}
+
+
+
+//let a = Array<Int>()
+//let b:Array<Int> = []
+//let c = [Int]()
+//let d: [Int] = []
+//
+//let a = Array(repeating: 4, count: 5)
+//print(a)
+//
+//var a: [Int] = [5, 4, 59, 4, 4]
+//let strArray:[String] = ["a", "c"]
+//
+//print(strArray)
+//
+//a.append(23)
+//
+//a += [2, 6]
+//
+//var b: [[[Int]]] = [
+//          [ [1, 4, 5], [2, 4, 4] ],
+//          [ [2, 5, 6], [6, 0, 5] ],
+//          [ [7, 8, 9], [78, 0, 6] ]
+//          ]
+//
+//var d = b[b.count - 1]
+//var c = d[d.count - 1]
+//print(c[c.count - 1])
+
